1013c0486a5c16d5e11158ac91ca8b39f1bb9061
Greg Robison <gjrobison64@gmail.com>
2017-01-26 15:30:45 -0500

    finished first question

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-// items.forEach(function(objects){
-//   console.log(objects);
-// });
-var priceList = items.map(function(priceFromList){
-console.log(priceFromList);
-  return [price];
+//this function maps the items list and pulls out the prices of each item and then
+//puts it into an array
+var prices = items.map(function(a){
 
-})
+  return a.price;
+});
+
+console.log(prices);
+
+//the next function adds up all the prices in the array and returns the total
+
+var sum = prices.reduce(function(subtotal, price){
+  return subtotal + price;
+});
+
+console.log(sum);
+
+//the next function takes the sum and divides it by the number of prices
+
+var average = function(x, y){
+  return x / y
+};
+
+console.log(average(sum, prices.length));
